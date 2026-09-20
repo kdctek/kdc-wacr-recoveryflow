d0a8ce4d189b56034143360dcb36880a30c3d661
KDC <69408642+kdctek@users.noreply.github.com>
2026-09-20 19:41:41 +0530

    Update release manifest test for RecoveryFlow name

--- tests/dist-manifest.php
+++ tests/dist-manifest.php
@@ -254,7 +254,7 @@ $pot = (string) file_get_contents( $root . '/languages/kdc-wacr-recoveryflow.pot
 
 dist_ok(
 	'the .pot names this version',
-	false !== strpos( $pot, 'KDC WAcr RecoveryFlow ' . $version )
+	false !== strpos( $pot, 'RecoveryFlow by WA.cr ' . $version )
 );
 
 /*
